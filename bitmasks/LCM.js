9de24492d9044d443fbff2c68eb9c2a6f30cd04b
kdm <kmalinich@luxotticaretail.com>
2016-06-03 00:43:53 -0400

    Update

--- bitmasks/LCM.js
+++ bitmasks/LCM.js
@@ -94,7 +94,8 @@ function offoff() {
 	var bit_5_test = bit_test(hex, bit_5);
 	var bit_6_test = bit_test(hex, bit_6);
 	var bit_7_test = bit_test(hex, bit_7);
-	var dsc ='superoff';
+
+	var dsc = 'superoff';
 	var hex = 0x00;
 
 	var string = dsc+'|'+clc.yellow(pad(hex, 3))+'|'+bit_0_test+'|'+bit_1_test+'|'+bit_2_test+'|'+bit_3_test+'|'+bit_4_test+'|'+bit_5_test+'|'+bit_6_test+'|'+bit_7_test;
@@ -117,32 +118,33 @@ function offoff() {
 }
 
 function bit_sample(dsc, hex, callback) {
+	var bit_0_test = bit_test(hex, bit_0);
+	var bit_1_test = bit_test(hex, bit_1);
+	var bit_2_test = bit_test(hex, bit_2);
+	var bit_3_test = bit_test(hex, bit_3);
+	var bit_4_test = bit_test(hex, bit_4);
+	var bit_5_test = bit_test(hex, bit_5);
+	var bit_6_test = bit_test(hex, bit_6);
+	var bit_7_test = bit_test(hex, bit_7);
+
+	var string = dsc+'|'+clc.yellow(pad(hex, 3))+'|'+bit_0_test+'|'+bit_1_test+'|'+bit_2_test+'|'+bit_3_test+'|'+bit_4_test+'|'+bit_5_test+'|'+bit_6_test+'|'+bit_7_test;
+	string     = string.replace(/true/g,  clc.green('TRU'));
+	string     = string.replace(/false/g, clc.red('FAL'));
+
+	console.log(string);
+
+	var src = 0x3F; // DIA
+	var dst = 0xBF; // GLO
+
+	var msg   = new Buffer([0x0c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, hex]);
+
+	var ibus_packet = {
+		src: src,
+		dst: dst,
+		msg: msg,
+	}
+
 	setTimeout(function() {
-		var bit_0_test = bit_test(hex, bit_0);
-		var bit_1_test = bit_test(hex, bit_1);
-		var bit_2_test = bit_test(hex, bit_2);
-		var bit_3_test = bit_test(hex, bit_3);
-		var bit_4_test = bit_test(hex, bit_4);
-		var bit_5_test = bit_test(hex, bit_5);
-		var bit_6_test = bit_test(hex, bit_6);
-		var bit_7_test = bit_test(hex, bit_7);
-
-		var string = dsc+'|'+clc.yellow(pad(hex, 3))+'|'+bit_0_test+'|'+bit_1_test+'|'+bit_2_test+'|'+bit_3_test+'|'+bit_4_test+'|'+bit_5_test+'|'+bit_6_test+'|'+bit_7_test;
-		string     = string.replace(/true/g,  clc.green('TRU'));
-		string     = string.replace(/false/g, clc.red('FAL'));
-
-		console.log(string);
-
-		var src = 0x00; // GM
-		var dst = 0xBF; // GLO
-		var msg = new Buffer([0x76, hex]);
-
-		var ibus_packet = {
-			src: src,
-			dst: dst,
-			msg: msg,
-		}
-
 		ibus_connection.send_message(ibus_packet);
 		callback(null, 'message sent');
 	}, 2000);
@@ -152,24 +154,13 @@ function do_sample() {
 	offoff();
 	var line       = '--------------------------------------------';
 	var header_dec = '             001|002|004|008|016|032|064|128';
-	var header     = 'Descr   |Val|IKE|Hzd|Low|Hig| 4 | 5 | 6 | 7 ';
+	var header     = 'Descr   |Val| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 ';
 
 	console.log(clc.yellow(header_dec));
 	console.log(clc.magenta(header));
 	console.log(line);
 
 	var result = wait.for(bit_sample, 'Off     ', 0x00);
-	var result = wait.for(bit_sample, 'Hazards ', 0x02);
-	// var result = wait.for(bit_sample, 'Lo beams', 0x04);
-	var result = wait.for(bit_sample, 'Hi beams', 0x08);
-	var result = wait.for(bit_sample, 'Hi+Hazrd', 0x0a);
-	var result = wait.for(bit_sample, 'Off     ', 0x00);
-
-	// var result = wait.for(bit_sample, 'Lo +Warn', 0x06);
-	// var result = wait.for(bit_sample, 'Hi+Warn ', 0x0A);
-	// var result = wait.for(bit_sample, 'Lo+Hi+Wr', 0x0E);
-	// var result = wait.for(bit_sample, 'Test1 ',   0x11);
-	// var result = wait.for(bit_sample, 'IKE     ', 0x01);
 }
 
 // startup();
@@ -180,13 +171,14 @@ function go() {
 
 // ibus_connection.on('port_open', go);
 
-// bit_sample('IKE Hzd', 0x01);
-// bit_sample('???    ', 0x03);
+// RR turn, R sidemarker
+var buffer_data   = 0x02;
+// Cluster
+var buffer_data   = 0x04;
+// RR turn, R tail, R sidemarker
+var buffer_data   = 0x0a;
 
-// bit_sample('Off     ', 0x00);
-
-
-lcm_bitmask_decode(0x32);
+lcm_bitmask_decode(buffer_data);
 // lcm_bitmask_decode(0x72);
 // lcm_bitmask_decode(0xB2);
 

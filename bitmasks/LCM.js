1795032c0f7280eba09928f30eb1ceda7b31e4c3
kdm <kmalinich@luxotticaretail.com>
2016-06-08 12:40:22 -0400

    Finished updating LCM.js from INPA strings, only suspect 3 values are incorrect

--- bitmasks/LCM.js
+++ bitmasks/LCM.js
@@ -131,32 +131,33 @@ function lcm_bitmask_encode(array) {
   if(array.output_lowbeam_front_left       ) { bitmask_5 = bit_set(bitmask_5, bit_4) ; }
   if(array.output_lowbeam_front_right      ) { bitmask_5 = bit_set(bitmask_5, bit_5) ; }
   if(array.output_fog_front_right          ) { bitmask_5 = bit_set(bitmask_5, bit_6) ; }
-
-
-	// Off by one
-  if(array.input_vertical_aim              ) { bitmask_5 = bit_set(bitmask_5, bit_7) ; }
-
   if(array.output_brake_rear_left          ) { bitmask_5 = bit_set(bitmask_5, bit_7) ; }
+  if(array.input_vertical_aim              ) { bitmask_6 = bit_set(bitmask_6, bit_1) ; }
+  if(array.output_license_rear_right       ) { bitmask_6 = bit_set(bitmask_6, bit_2) ; }
+  if(array.output_standing_rear_left       ) { bitmask_6 = bit_set(bitmask_6, bit_3) ; }
+  if(array.output_brake_rear_middle        ) { bitmask_6 = bit_set(bitmask_6, bit_4) ; }
+  if(array.output_standing_front_right     ) { bitmask_6 = bit_set(bitmask_6, bit_5) ; }
+  if(array.output_turn_front_right         ) { bitmask_6 = bit_set(bitmask_6, bit_6) ; }
+  if(array.output_turn_rear_left           ) { bitmask_6 = bit_set(bitmask_6, bit_7) ; }
+  if(array.output_turn_rear_right          ) { bitmask_7 = bit_set(bitmask_7, bit_1) ; }
+  if(array.output_fog_rear_left            ) { bitmask_7 = bit_set(bitmask_7, bit_2) ; }
+  if(array.output_standing_inner_rear_right) { bitmask_7 = bit_set(bitmask_7, bit_3) ; }
+  if(array.output_standing_rear_right      ) { bitmask_7 = bit_set(bitmask_7, bit_4) ; }
+  if(array.output_turn_front_left          ) { bitmask_7 = bit_set(bitmask_7, bit_6) ; }
+  if(array.output_reverse_rear_right       ) { bitmask_7 = bit_set(bitmask_7, bit_7) ; }
+  if(array.mode_failsafe                   ) { bitmask_8 = bit_set(bitmask_8, bit_0) ; }
+  if(array.output_led_switch_hazard        ) { bitmask_8 = bit_set(bitmask_8, bit_2) ; }
+  if(array.output_led_switch_light         ) { bitmask_8 = bit_set(bitmask_8, bit_3) ; }
+  if(array.output_reverse_rear_trailer     ) { bitmask_8 = bit_set(bitmask_8, bit_5) ; }
+  if(array.mode_sleep                      ) { bitmask_8 = bit_set(bitmask_8, bit_6) ; }
 
-  if(array.output_license_rear_right       ) { bitmask_6 = bit_set(bitmask_6, bit_1) ; }
-  if(array.output_standing_rear_left       ) { bitmask_6 = bit_set(bitmask_6, bit_2) ; }
-  if(array.output_brake_rear_middle        ) { bitmask_6 = bit_set(bitmask_6, bit_3) ; }
-  if(array.output_standing_front_right     ) { bitmask_6 = bit_set(bitmask_6, bit_4) ; }
-  if(array.output_turn_front_right         ) { bitmask_6 = bit_set(bitmask_6, bit_5) ; }
-  if(array.output_turn_rear_left           ) { bitmask_6 = bit_set(bitmask_6, bit_6) ; }
-  if(array.output_turn_rear_right          ) { bitmask_6 = bit_set(bitmask_6, bit_7) ; }
-  if(array.output_fog_rear_left            ) { bitmask_7 = bit_set(bitmask_7, bit_1) ; }
-  if(array.output_standing_inner_rear_right) { bitmask_7 = bit_set(bitmask_7, bit_2) ; }
-  if(array.output_standing_rear_right      ) { bitmask_7 = bit_set(bitmask_7, bit_3) ; }
-  if(array.output_turn_front_left          ) { bitmask_7 = bit_set(bitmask_7, bit_4) ; }
-  if(array.output_reverse_rear_right       ) { bitmask_7 = bit_set(bitmask_7, bit_6) ; }
-  if(array.mode_failsafe                   ) { bitmask_7 = bit_set(bitmask_7, bit_7) ; }
-  if(array.output_led_switch_hazard        ) { bitmask_8 = bit_set(bitmask_8, bit_0) ; }
-  if(array.output_led_switch_light         ) { bitmask_8 = bit_set(bitmask_8, bit_2) ; }
-  if(array.output_reverse_rear_trailer     ) { bitmask_8 = bit_set(bitmask_8, bit_3) ; }
-  if(array.mode_sleep                      ) { bitmask_8 = bit_set(bitmask_8, bit_5) ; }
-  if(array.output_fog_rear_right           ) { bitmask_8 = bit_set(bitmask_8, bit_6) ; }
 
+	// Suspect	
+	// array.clamp_58g
+	// array.output_fog_rear_right
+	// array.output_fog_rear_trailer
+
+	// ?? 
   // if(array.) { bitmask_0 = bit_set(bitmask_0, bit_3) ; }
   // if(array.) { bitmask_0 = bit_set(bitmask_0, bit_5) ; }
   // if(array.) { bitmask_1 = bit_set(bitmask_1, bit_3) ; }
@@ -188,9 +189,7 @@ function lcm_bitmask_encode(array) {
 		bitmask_11,
   ];
 
-	output = output.concat([0x00, 0xE4, 0xFF, 0x00]);
-
-	console.log(output);
+	console.log('lcm_bitmask_encode() output: %s', output);
   return output;
 }
 
@@ -205,65 +204,69 @@ function lcm_bitmask_decode(array) {
   var bitmask_6 = array[6];
   var bitmask_7 = array[7];
 
-  // 0
-  var clamp_15                        = bit_test(bitmask_3, bit_5);
-  var clamp_30a                       = bit_test(bitmask_0, bit_0);
-  var clamp_30b                       = bit_test(bitmask_0, bit_7);
-  var clamp_r                         = bit_test(bitmask_0, bit_6);
-  var input_air_suspension            = bit_test(bitmask_3, bit_0);
-  var input_armoured_door             = bit_test(bitmask_1, bit_6);
-  var input_brake_fluid_level         = bit_test(bitmask_1, bit_7);
-  var input_carb                      = bit_test(bitmask_0, bit_4);
-  var input_engine_failsafe           = bit_test(bitmask_3, bit_6);
-  var input_fire_extinguisher         = bit_test(bitmask_0, bit_1);
-  var input_hold_up_alarm             = bit_test(bitmask_3, bit_1);
-  var input_key_in_ignition           = bit_test(bitmask_1, bit_0);
-  var input_kfn                       = bit_test(bitmask_1, bit_5);
-  var input_preheating_fuel_injection = bit_test(bitmask_0, bit_2);
-  var input_seat_belts_lock           = bit_test(bitmask_1, bit_1);
-  var input_tire_defect               = bit_test(bitmask_3, bit_7);
-  var input_vertical_aim              = bit_test(bitmask_5, bit_7);
-  var input_washer_fluid_level        = bit_test(bitmask_3, bit_2);
-  var mode_failsafe                   = bit_test(bitmask_7, bit_7);
-  var mode_sleep                      = bit_test(bitmask_8, bit_5);
-  var output_brake_rear_middle        = bit_test(bitmask_6, bit_3);
-  var output_brake_rear_right         = bit_test(bitmask_4, bit_4);
-  var output_fog_front_left           = bit_test(bitmask_5, bit_2);
-  var output_fog_front_right          = bit_test(bitmask_5, bit_6);
-  var output_fog_rear_left            = bit_test(bitmask_7, bit_1);
-  var output_fog_rear_right           = bit_test(bitmask_8, bit_6);
-  var output_highbeam_front_left      = bit_test(bitmask_4, bit_6);
-  var output_highbeam_front_right     = bit_test(bitmask_4, bit_5);
-  var output_led_switch_hazard        = bit_test(bitmask_8, bit_0);
-  var output_led_switch_light         = bit_test(bitmask_8, bit_2);
-  var output_license_rear_left        = bit_test(bitmask_4, bit_2);
-  var output_license_rear_right       = bit_test(bitmask_6, bit_1);
-  var output_lowbeam_front_left       = bit_test(bitmask_5, bit_4);
-  var output_lowbeam_front_right      = bit_test(bitmask_5, bit_5);
-  var output_reverse_rear_left        = bit_test(bitmask_5, bit_3);
-  var output_reverse_rear_right       = bit_test(bitmask_7, bit_6);
-  var output_reverse_rear_trailer     = bit_test(bitmask_8, bit_3);
-  var output_standing_front_left      = bit_test(bitmask_5, bit_0);
-  var output_standing_front_right     = bit_test(bitmask_6, bit_4);
-  var output_standing_inner_rear_left = bit_test(bitmask_5, bit_1);
-  var output_standing_inner_rear_right= bit_test(bitmask_7, bit_2);
-  var output_standing_rear_left       = bit_test(bitmask_6, bit_2);
-  var output_standing_rear_right      = bit_test(bitmask_7, bit_3);
-  var output_turn_front_left          = bit_test(bitmask_7, bit_4);
-  var output_turn_front_right         = bit_test(bitmask_6, bit_5);
-  var output_turn_rear_left           = bit_test(bitmask_6, bit_6);
-  var output_turn_rear_right          = bit_test(bitmask_6, bit_7);
-  var switch_brake                    = bit_test(bitmask_2, bit_0);
-  var switch_fog_front                = bit_test(bitmask_2, bit_2);
-  var switch_fog_rear                 = bit_test(bitmask_2, bit_4);
-  var switch_hazard                   = bit_test(bitmask_1, bit_4);
-  var switch_highbeam                 = bit_test(bitmask_2, bit_1);
-  var switch_highbeam_flash           = bit_test(bitmask_1, bit_2);
-  var switch_lowbeam_1                = bit_test(bitmask_3, bit_4);
-  var switch_lowbeam_2                = bit_test(bitmask_3, bit_3);
-  var switch_standing                 = bit_test(bitmask_2, bit_5);
-  var switch_turn_left                = bit_test(bitmask_2, bit_7);
-  var switch_turn_right               = bit_test(bitmask_2, bit_6);
+  var clamp_15                         = bit_test(bitmask_3, bit_5);
+  var clamp_30a                        = bit_test(bitmask_0, bit_0);
+  var clamp_30b                        = bit_test(bitmask_0, bit_7);
+  var clamp_r                          = bit_test(bitmask_0, bit_6);
+  var input_air_suspension             = bit_test(bitmask_3, bit_0);
+  var input_armoured_door              = bit_test(bitmask_1, bit_6);
+  var input_brake_fluid_level          = bit_test(bitmask_1, bit_7);
+  var input_carb                       = bit_test(bitmask_0, bit_4);
+  var input_engine_failsafe            = bit_test(bitmask_3, bit_6);
+  var input_fire_extinguisher          = bit_test(bitmask_0, bit_1);
+  var input_hold_up_alarm              = bit_test(bitmask_3, bit_1);
+  var input_key_in_ignition            = bit_test(bitmask_1, bit_0);
+  var input_kfn                        = bit_test(bitmask_1, bit_5);
+  var input_preheating_fuel_injection  = bit_test(bitmask_0, bit_2);
+  var input_seat_belts_lock            = bit_test(bitmask_1, bit_1);
+  var input_tire_defect                = bit_test(bitmask_3, bit_7);
+  var input_vertical_aim               = bit_test(bitmask_6, bit_1);
+  var input_washer_fluid_level         = bit_test(bitmask_3, bit_2);
+  var mode_failsafe                    = bit_test(bitmask_8, bit_0);
+  var mode_sleep                       = bit_test(bitmask_8, bit_6);
+  var output_brake_rear_left           = bit_test(bitmask_5, bit_7);
+  var output_brake_rear_middle         = bit_test(bitmask_6, bit_4);
+  var output_brake_rear_right          = bit_test(bitmask_4, bit_4);
+  var output_fog_front_left            = bit_test(bitmask_5, bit_2);
+  var output_fog_front_right           = bit_test(bitmask_5, bit_6);
+  var output_fog_rear_left             = bit_test(bitmask_7, bit_2);
+  var output_highbeam_front_left       = bit_test(bitmask_4, bit_6);
+  var output_highbeam_front_right      = bit_test(bitmask_4, bit_5);
+  var output_led_switch_hazard         = bit_test(bitmask_8, bit_2);
+  var output_led_switch_light          = bit_test(bitmask_8, bit_3);
+  var output_license_rear_left         = bit_test(bitmask_4, bit_2);
+  var output_license_rear_right        = bit_test(bitmask_6, bit_2);
+  var output_lowbeam_front_left        = bit_test(bitmask_5, bit_4);
+  var output_lowbeam_front_right       = bit_test(bitmask_5, bit_5);
+  var output_reverse_rear_left         = bit_test(bitmask_5, bit_3);
+  var output_reverse_rear_right        = bit_test(bitmask_7, bit_7);
+  var output_reverse_rear_trailer      = bit_test(bitmask_8, bit_5);
+  var output_standing_front_left       = bit_test(bitmask_5, bit_0);
+  var output_standing_front_right      = bit_test(bitmask_6, bit_5);
+  var output_standing_inner_rear_left  = bit_test(bitmask_5, bit_1);
+  var output_standing_inner_rear_right = bit_test(bitmask_7, bit_3);
+  var output_standing_rear_left        = bit_test(bitmask_6, bit_3);
+  var output_standing_rear_right       = bit_test(bitmask_7, bit_4);
+  var output_turn_front_left           = bit_test(bitmask_7, bit_6);
+  var output_turn_front_right          = bit_test(bitmask_6, bit_6);
+  var output_turn_rear_left            = bit_test(bitmask_6, bit_7);
+  var output_turn_rear_right           = bit_test(bitmask_7, bit_1);
+  var switch_brake                     = bit_test(bitmask_2, bit_0);
+  var switch_fog_front                 = bit_test(bitmask_2, bit_2);
+  var switch_fog_rear                  = bit_test(bitmask_2, bit_4);
+  var switch_hazard                    = bit_test(bitmask_1, bit_4);
+  var switch_highbeam                  = bit_test(bitmask_2, bit_1);
+  var switch_highbeam_flash            = bit_test(bitmask_1, bit_2);
+  var switch_lowbeam_1                 = bit_test(bitmask_3, bit_4);
+  var switch_lowbeam_2                 = bit_test(bitmask_3, bit_3);
+  var switch_standing                  = bit_test(bitmask_2, bit_5);
+  var switch_turn_left                 = bit_test(bitmask_2, bit_7);
+  var switch_turn_right                = bit_test(bitmask_2, bit_6);
+
+	// Suspect
+  // var clamp_58g                       = bit_test(bitmask_, bit_);
+  // var output_fog_rear_right           = bit_test(bitmask_, bit_);
+  // var output_fog_rear_trailer         = bit_test(bitmask_, bit_);
 
   var output = {
 		clamp_15                         : clamp_15,
@@ -291,8 +294,6 @@ function lcm_bitmask_decode(array) {
 		output_fog_front_left            : output_fog_front_left,
 		output_fog_front_right           : output_fog_front_right,
 		output_fog_rear_left             : output_fog_rear_left,
-		output_fog_rear_right            : output_fog_rear_right,
-		output_fog_rear_trailer          : output_fog_rear_trailer,
 		output_highbeam_front_left       : output_highbeam_front_left,
 		output_highbeam_front_right      : output_highbeam_front_right,
 		output_led_switch_hazard         : output_led_switch_hazard,
@@ -327,6 +328,11 @@ function lcm_bitmask_decode(array) {
 		switch_turn_right                : switch_turn_right,
   }
 
+	// Suspect
+	// clamp_58g                        : clamp_58g,
+	// output_fog_rear_right            : output_fog_rear_right,
+	// output_fog_rear_trailer          : output_fog_rear_trailer,
+
   return output;
 }
 
@@ -352,12 +358,12 @@ var array_of_possible_values = {
   input_washer_fluid_level         : true,
   mode_failsafe                    : true,
   mode_sleep                       : true,
+  output_brake_rear_left           : true,
   output_brake_rear_middle         : true,
   output_brake_rear_right          : true,
   output_fog_front_left            : true,
   output_fog_front_right           : true,
   output_fog_rear_left             : true,
-  output_fog_rear_right            : true,
   output_highbeam_front_left       : true,
   output_highbeam_front_right      : true,
   output_led_switch_hazard         : true,
@@ -390,6 +396,11 @@ var array_of_possible_values = {
   switch_standing                  : true,
   switch_turn_left                 : true,
   switch_turn_right                : true,
+
+	// Suspect
+  // clamp_58g                        : true,
+  // output_fog_rear_right            : true,
+  // output_fog_rear_trailer          : true,
 }
 
 function go() {

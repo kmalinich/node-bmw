bb12921197a2c1128c6a028234d8c3507d663141
kdm <kmalinich@luxotticaretail.com>
2016-07-22 01:25:56 -0400

    Update

--- bitmasks/BMBT.js
+++ bitmasks/BMBT.js
@@ -8,14 +8,14 @@ var clc = require('cli-color');
 
 
 // Bitmasks in hex
-var bit_0 = 0x01;
-var bit_1 = 0x02;
-var bit_2 = 0x04;
-var bit_3 = 0x08;
-var bit_4 = 0x10;
-var bit_5 = 0x20;
-var bit_6 = 0x40;
-var bit_7 = 0x80;
+var bit_0 = 0x01; // 1
+var bit_1 = 0x02; // 2
+var bit_2 = 0x04; // 4
+var bit_3 = 0x08; // 8
+var bit_4 = 0x10; // 16
+var bit_5 = 0x20; // 32
+var bit_6 = 0x40; // 64
+var bit_7 = 0x80; // 128
 
 // Bitmask constants
 var hold    = bit_6;

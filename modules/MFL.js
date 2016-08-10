5c1c1f8b6434bf43b50a5cea08eba224563daf62
kdm <kmalinich@luxotticaretail.com>
2016-08-09 22:27:19 -0400

    Added AVRCP play/pause to MFL, cleaned up logs and reference folder docs

--- modules/MFL.js
+++ modules/MFL.js
@@ -17,121 +17,150 @@ var bit_7 = 0x80; // 128
 
 // Test number for bitmask
 function bit_test(num, bit) {
-	if ((num & bit) != 0) { return true; }
-	else { return false; }
+  if ((num & bit) != 0) { return true; }
+  else { return false; }
 }
 
 
+// Send commands over Linux DBus to control A2DP connected device
+function control_avrcp(action) {
+  switch (action) {
+    case 'pause':
+      // Send previous track command to BlueZ
+      omnibus.system_bus.invoke({
+        path        : '/org/bluez/hci0/dev_EC_88_92_5E_5D_36/player0',
+        destination : 'org.bluez',
+        'interface' : 'org.bluez.MediaPlayer1',
+        member      : 'Pause',
+        type        : dbus.messageType.methodCall
+      });
+      break;
+
+    case 'play':
+      // Send previous track command to BlueZ
+      omnibus.system_bus.invoke({
+        path        : '/org/bluez/hci0/dev_EC_88_92_5E_5D_36/player0',
+        destination : 'org.bluez',
+        'interface' : 'org.bluez.MediaPlayer1',
+        member      : 'Play',
+        type        : dbus.messageType.methodCall
+      });
+      break;
+
+    case 'previous':
+      // Send previous track command to BlueZ
+      omnibus.system_bus.invoke({
+        path        : '/org/bluez/hci0/dev_EC_88_92_5E_5D_36/player0',
+        destination : 'org.bluez',
+        'interface' : 'org.bluez.MediaPlayer1',
+        member      : 'Previous',
+        type        : dbus.messageType.methodCall
+      });
+      break;
+
+    case 'next':
+      // Send next track command to BlueZ
+      omnibus.system_bus.invoke({
+        path        : '/org/bluez/hci0/dev_EC_88_92_5E_5D_36/player0',
+        destination : 'org.bluez',
+        'interface' : 'org.bluez.MediaPlayer1',
+        member      : 'Next',
+        type        : dbus.messageType.methodCall
+      });
+      break;
+  }
+
+  console.log('[MFL]  Sending \'%s\' command over system bus', action);
+}
+
 var MFL = function(omnibus) {
 
-	// Self reference
-	var _self = this;
+  // Self reference
+  var _self = this;
 
-	// Exposed data
-	this.parse_data = parse_data;
+  // Exposed data
+  this.parse_data = parse_data;
 
 
-	// Parse data sent by real MFL module
-	function parse_data(message) {
-		// Init variables
-		var action;
-		var button;
-		var command;
+  // Parse data sent by real MFL module
+  function parse_data(message) {
+    // Init variables
+    var action;
+    var button;
+    var command;
 
-		// Volume buttons
-		if (message[0] == 0x32) {
-			command = 'button action';
-			button  = 'volume';
+    // 50 B0 01,MFL --> SES: Device status request
+    // 50 C8 01,MFL --> TEL: Device status request
 
-			// Needs to be finished.. I think
-			if      (message[1] == 0x10) { action = 'decrease 1 step'; }
-			else if (message[1] == 0x11) { action = 'increase 1 step'; }
-		}
+    switch (message[0]) {
+      case 0x32: // Volume buttons
+        command = 'button action';
+        button  = 'volume';
 
-		// Recirc button
-		else if (message[0] == 0x3A) {
-			command = 'button action';
-			button  = 'recirculation';
+        switch (message[1]) {
+          case 0x10:
+            action = 'decrease 1 step';
+            break;
+          case 0x11:
+            action = 'increase 1 step';
+            break;
+        }
+        break;
 
-			// Bitmask:
-			// 0x00 = released
-			// 0x08 = pressed
+      case 0x3A: // Recirculation buton
+        command = 'button action';
+        button  = 'recirculation';
 
-			if      (message[1] == 0x00) { action = 'released';  }
-			else if (message[1] == 0x01) { action = 'depressed'; }
-		}
+        switch (message[1]) {
+          case 0x00:
+            action = 'released';
+            break;
+          case 0x08:
+            action = 'depressed';
+            break;
+        }
 
-		// Media control buttons
-		else if (message[0] == 0x3B) {
-			command = 'button action';
+        break;
 
-			// Bitmask:
-			// 0x00 = no buttons pressed
-			// 0x01 = right
-			// 0x08 = left
-			// 0x10 = long depress
-			// 0x20 = release
-			// 0x80 = send/end
+        // Media control buttons
+      case 0x3B: // Media control buttons
+        command = 'button action';
 
-			// Detect button
-			if      (bit_test(message[1], bit_0)) { button = 'right';    }
-			else if (bit_test(message[1], bit_3)) { button = 'left';     }
-			else if (bit_test(message[1], bit_7)) { button = 'send/end'; }
-			else {
-				button = 'unknown';
-			}
+        // Bitmask:
+        // 0x00 = no buttons pressed
+        // 0x01 = right
+        // 0x08 = left
+        // 0x10 = long depress
+        // 0x20 = release
+        // 0x80 = send/end
 
-			// Detect action
-			if      (bit_test(message[1], bit_4)) { action = 'long depress'; }
-			else if (bit_test(message[1], bit_5)) { action = 'release';      }
-			else {
-				action = 'depress';
-			}
+        // Detect button
+        if      (bit_test(message[1], bit_0)) { button = 'right';    }
+        else if (bit_test(message[1], bit_3)) { button = 'left';     }
+        else if (bit_test(message[1], bit_7)) { button = 'send/end'; }
+        else                                  { button = 'unknown';  }
 
-			if (button == 'left' && action == 'depress') {
-				console.log('[MFL]  Sending previous track command over system bus');
+        // Detect action
+        if      (bit_test(message[1], bit_4)) { action = 'long depress'; }
+        else if (bit_test(message[1], bit_5)) { action = 'release';      }
+        else                                  { action = 'depress';      }
 
-				// Send previous track command to BlueZ
-				omnibus.system_bus.invoke({
-					path        : '/org/bluez/hci0/dev_EC_88_92_5E_5D_36/player0',
-					destination : 'org.bluez',
-					'interface' : 'org.bluez.MediaPlayer1',
-					member      : 'Previous',
-					type        : dbus.messageType.methodCall
-				});
-			}
+        // Perform media control based on pressed key
+        if      (button == 'left'     && action == 'depress')      { control_avrcp('previous'); }
+        else if (button == 'right'    && action == 'depress')      { control_avrcp('next');     }
+        else if (button == 'send/end' && action == 'depress')      { control_avrcp('pause');    } // Think about it...
+        else if (button == 'send/end' && action == 'long depress') { control_avrcp('play');     }
+        break;
 
-			else if (button == 'right' && action == 'depress') {
-				console.log('[MFL]  Sending next track command over system bus');
+      case default:
+        command = 'unknown';
+        button  = 'unknown';
+        action  = new Buffer(message);
+        break;
+    }
 
-				// Send next track command to BlueZ
-				omnibus.system_bus.invoke({
-					path        : '/org/bluez/hci0/dev_EC_88_92_5E_5D_36/player0',
-					destination : 'org.bluez',
-					'interface' : 'org.bluez.MediaPlayer1',
-					member      : 'Next',
-					type        : dbus.messageType.methodCall
-				});
-			}
-		}
-
-		// Nope..
-		// 50 B0 01,MFL --> SES: Device status request
-		// 50 C8 01,MFL --> TEL: Device status request
-		// else if (message[0] == 0x01) {
-		//  command = 'button action';
-		//  button  = 'r/t';
-		//  action  = 'depress';
-		// }
-
-		else {
-			command = 'unknown';
-			button  = 'unknown';
-			action  = new Buffer(message);
-		}
-
-		console.log('[MFL]  Sent %s: %s', command, button, action);
-	}
+    console.log('[MFL]  Sent %s: %s', command, button, action);
+  }
 }
 
 module.exports = MFL;

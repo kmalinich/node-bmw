fe24927d17bac258fba860b7256235ebd7478f02
kdm <kmalinich@luxotticaretail.com>
2016-08-11 02:44:53 -0400

    Testing of DSP control code works

--- ref/dsp/dsp-test.js
+++ ref/dsp/dsp-test.js
@@ -10,10 +10,24 @@ function bit_test(num, bit) {
 var data_1;
 var data_2;
 var data_3;
+var data_4;
+var data_5;
+var data_6;
+var data_7;
 var dsp_mode;
 var echo;
 var reverb;
 var room_size;
+
+var dsp_modes = {
+	0 : 'concert hall',
+	1 : 'jazz club',
+	2 : 'cathedral',
+	3 : 'memory 1',
+	4 : 'memory 2',
+	5 : 'memory 3',
+	6 : 'DSP off',
+}
 
 function decode_dsp(data) {
   dsp_mode  = data[1] - 1;
@@ -39,17 +53,17 @@ function decode_dsp(data) {
     }
   }
 
-  console.log('DSP mode  : %s', dsp_mode);
-  console.log('Reverb    : %s', reverb);
-  console.log('Room size : %s', room_size);
+  console.log('DSP mode  : %s', dsp_modes[dsp_mode]);
+  console.log('Reverb    : %s/10', reverb);
+  console.log('Room size : %s/10', room_size);
   console.log('----------------');
-  console.log('80Hz      : %s', band[0]);
-  console.log('200Hz     : %s', band[1]);
-  console.log('500Hz     : %s', band[2]);
-  console.log('1KHz      : %s', band[3]);
-  console.log('2KHz      : %s', band[4]);
-  console.log('5KHz      : %s', band[5]);
-  console.log('12KHz     : %s', band[6]);
+  console.log('80Hz      : %s/10', band[0]);
+  console.log('200Hz     : %s/10', band[1]);
+  console.log('500Hz     : %s/10', band[2]);
+  console.log('1KHz      : %s/10', band[3]);
+  console.log('2KHz      : %s/10', band[4]);
+  console.log('5KHz      : %s/10', band[5]);
+  console.log('12KHz     : %s/10', band[6]);
   console.log('----------------');
 
   console.log(data);
@@ -60,14 +74,13 @@ function encode_dsp(data) {
   var reverb_out    = [0x34, 0x94 + data.memory, data.reverb & 0x0F];
   var room_size_out = [0x34, 0x94 + data.memory, (data.room_size & 0x0F) | 0x20];
 
-  console.log(memory);
-  console.log(reverb_out);
-  console.log(room_size_out);
+  console.log(new Buffer(reverb_out));
+  console.log(new Buffer(room_size_out));
 
   for (var band_num = 0; band_num < 7; band_num++) {
     // ... Don't look at me...
     var band_out = [0x34, 0x14 + data.memory, (((band_num * 2) << 4) & 0xF0) | ((data.band[band_num] < 0 ? (0x10 | (Math.abs(data.band[band_num]) & 0x0F)) : (data.band[band_num] & 0x0F)))];
-    console.log(band_out);
+    console.log(new Buffer(band_out));
   }
 
 }
@@ -75,23 +88,30 @@ function encode_dsp(data) {
 data_1 = [0x35, 0x04, 0x04, 0x21, 0x0A, 0x27, 0x45, 0x71, 0x84, 0xA9, 0xCA];
 data_2 = [0x35, 0x04, 0x04, 0x23, 0x05, 0x23, 0x41, 0x72, 0x91, 0xA9, 0xC9];
 data_3 = [0x35, 0x05, 0x01, 0x23, 0x08, 0x22, 0x42, 0x61, 0x82, 0xA5, 0xC8];
+data_4 = [0x35, 0x04, 0x00, 0x23, 0x03, 0x27, 0x48, 0x67, 0x8a, 0xA9, 0xC8]; // kdm-e39 mode 4
+data_5 = [0x35, 0x06, 0x00, 0x20, 0x00, 0x20, 0x40, 0x60, 0x80, 0xA0, 0xC0]; // kdm-e39 mode 
+data_6 = [0x35, 0x05, 0x00, 0x20, 0x0A, 0x27, 0x42, 0x77, 0x93, 0xA6, 0xCA]; // kdm-e39 - good preset
+data_7 = [0x35, 0x04, 0x01, 0x23, 0x03, 0x27, 0x48, 0x67, 0x8A, 0xA9, 0xC8];
 
 // decode_dsp(data_1);
 // decode_dsp(data_2);
 // decode_dsp(data_3);
+// decode_dsp(data_4);
+// decode_dsp(data_5);
+decode_dsp(data_6);
 
 var dsp_data = {
-  memory    : 1,
+  memory    : 2,
   reverb    : 0,
   room_size : 0,
-  band : {
-    0 : 0,
-    1 : 0,
-    2 : 0,
-    3 : 0,
-    4 : 0,
-    5 : 0,
-    6 : 0,
+  band      : {
+    0 : 10,
+    1 : 7,
+    2 : 2,
+    3 : -7,
+    4 : -3,
+    5 : 6,
+    6 : 10,
   },
 }
 

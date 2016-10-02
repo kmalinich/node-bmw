2cd0df39f8c39d12273d301eb964a5e6de69808b
kdm <kmalinich@luxotticaretail.com>
2016-10-01 20:50:41 -0400

    Update

--- ibus/data-handler.js
+++ ibus/data-handler.js
@@ -22,29 +22,30 @@ var data_handler = function(omnibus) {
 
 			// Send message to code module to parse
 			switch (data.src_name) {
-				case 'ABG'  : omnibus.ABG.parse_data(data.msg)  ; break;
-				case 'ANZV' : omnibus.ANZV.parse_data(data.msg) ; break;
-				case 'BMBT' : omnibus.BMBT.parse_data(data.msg) ; break;
-				case 'CCM'  : omnibus.CCM.parse_data(data.msg)  ; break;
-				case 'CDC'  : omnibus.CDC.parse_data(data.msg)  ; break;
-				case 'DSP'  : omnibus.DSP.parse_data(data.msg)  ; break;
-				case 'DSPC' : omnibus.DSPC.parse_out(data)      ; break;
-				case 'EWS'  : omnibus.EWS.parse_data(data.msg)  ; break;
-				case 'GM'   : omnibus.GM.parse_data(data.msg)   ; break;
-				case 'GT'   : omnibus.GT.parse_data(data.msg)   ; break;
-				case 'HAC'  : omnibus.HAC.parse_data(data.msg)  ; break;
-				case 'IHKA' : omnibus.IHKA.parse_data(data.msg) ; break;
-				case 'IKE'  : omnibus.IKE.parse_data(data.msg)  ; break;
-				case 'LCM'  : omnibus.LCM.parse_data(data.msg)  ; break;
-				case 'MFL'  : omnibus.MFL.parse_data(data.msg)  ; break;
-				case 'MID'  : omnibus.MID.parse_data(data.msg)  ; break;
-				case 'NAV'  : omnibus.NAV.parse_data(data.msg)  ; break;
-				case 'PDC'  : omnibus.PDC.parse_data(data.msg)  ; break;
-				case 'RAD'  : omnibus.RAD.parse_data(data)      ; break;
-				case 'RLS'  : omnibus.RLS.parse_data(data.msg)  ; break;
-				case 'SES'  : omnibus.SES.parse_data(data.msg)  ; break;
-				case 'SHD'  : omnibus.SHD.parse_data(data.msg)  ; break;
-				case 'TEL'  : omnibus.TEL.parse_data(data.msg)  ; break;
+				case 'ABG'  : omnibus.ABG.parse_data(data.msg);  break;
+				case 'ANZV' : omnibus.ANZV.parse_data(data.msg); break;
+				case 'BMBT' : omnibus.BMBT.parse_data(data.msg); break;
+				case 'CCM'  : omnibus.CCM.parse_data(data.msg);  break;
+				case 'CDC'  : omnibus.CDC.parse_data(data.msg);  break;
+				case 'DSP'  : omnibus.DSP.parse_data(data.msg);  break;
+				case 'DSPC' : omnibus.DSPC.parse_out(data);      break;
+				case 'EWS'  : omnibus.EWS.parse_data(data.msg);  break;
+				case 'GM'   : omnibus.GM.parse_data(data.msg);   break;
+				case 'GT'   : omnibus.GT.parse_data(data.msg);   break;
+				case 'HAC'  : omnibus.HAC.parse_data(data.msg);  break;
+				case 'IHKA' : omnibus.IHKA.parse_data(data.msg); break;
+				case 'IKE'  : omnibus.IKE.parse_data(data.msg);  break;
+				case 'LCM'  : omnibus.LCM.parse_data(data.msg);  break;
+				case 'MFL'  : omnibus.MFL.parse_data(data.msg);  break;
+				case 'MID'  : omnibus.MID.parse_data(data.msg);  break;
+				case 'NAV'  : omnibus.NAV.parse_data(data.msg);  break;
+				case 'PDC'  : omnibus.PDC.parse_data(data.msg);  break;
+				case 'RAD'  : omnibus.RAD.parse_data(data);      break;
+				case 'RLS'  : omnibus.RLS.parse_data(data.msg);  break;
+				case 'SES'  : omnibus.SES.parse_data(data.msg);  break;
+				case 'SHD'  : omnibus.SHD.parse_data(data.msg);  break;
+				case 'TEL'  : omnibus.TEL.parse_data(data.msg);  break;
+				case 'DIA'  : console.log('[%s->%s] Diag command', data.src_name, data.dst_name, data.msg); break;
 				default     : console.log('[%s->%s] No source handler', data.src_name, data.dst_name, data.msg);
 			}
 

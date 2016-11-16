def35895de6f3968fb415bbdcff93e46370f560c
kdm <kennygprs@gmail.com>
2016-11-15 22:45:21 -0500

    Update

--- ibus/ibus-protocol.js
+++ ibus/ibus-protocol.js
@@ -5,22 +5,90 @@ module.exports = {
 		emitter.emit('data', buffer);
 	},
 
-  // Emit a data event every `length` bytes
-  parser: function(length) {
-    var data = new Buffer(0);
-    console.log('[parser] External buffer         : ', data);
+	// Emit a data event every `length` bytes
+	parser: function(length) {
+		var data = new Buffer(0);
 
-    return function(emitter, buffer) {
-      data = Buffer.concat([data, buffer]);
-      console.log('[parser] Internal buffer         : ', data);
-  
-      while (data.length >= length) {
-        var out = data.slice(0, length);
-        data = data.slice(length);
-        emitter.emit('data', out);
-      }
-    };
-  },
+		return function(emitter, buffer) {
+			data = Buffer.concat([data, buffer]);
+			console.log('[parser] Current buffer         : ', data);
+
+			while (data.length >= length) {
+				console.log('[parser] Analyzing chunk        : ', data);
+
+				// Gather messages from current chunk
+				var messages = [];
+
+				var end_of_last_message = -1;
+
+				var mSrc;
+				var mLen;
+				var mDst;
+				var mMsg;
+				var mCrc;
+
+				// Look for messages in current chunk
+				for (var i = 0; i < data.length - 5; i++) {
+					mSrc = data[i + 0];
+					mLen = data[i + 1];
+					mDst = data[i + 2];
+
+					// Test to see if have enough data for a complete message
+					if (data.length >= (i + 2 + mLen)) {
+						mMsg = data.slice(i + 3, i + 3 + mLen - 2);
+						mCrc = data[i + 2 + mLen - 1];
+
+						var crc = 0x00;
+
+						crc = crc ^ mSrc;
+						crc = crc ^ mLen;
+						crc = crc ^ mDst;
+
+						for (var j = 0; j < mMsg.length; j++) {
+							crc = crc ^ mMsg[j];
+						}
+
+						if (crc === mCrc) {
+							messages.push({
+								'id'  : Date.now(),
+								'src' : mSrc.toString(16),
+								'len' : mLen.toString(16),
+								'dst' : mDst.toString(16),
+								'msg' : mMsg,
+								'crc' : mCrc.toString(16)
+							});
+
+							// Mark end of last message
+							end_of_last_message = (i + 2 + mLen);
+
+							// Skip ahead
+							i = end_of_last_message - 1;
+						}
+					}
+				}
+
+				if (messages.length > 0) {
+					messages.forEach(function(message) {
+						var out = data.slice(0, length);
+						//data = data.slice(length);
+						emitter.emit('data', out);
+
+						console.log('[parser] Emitting message       : ', message.src, message.len, message.dst, message.msg, message.crc);
+					});
+				}
+
+				// Push the remaining data back to the stream
+				if (end_of_last_message !== -1) {
+					// Push the remaining chunk from the end of the last valid message
+					date = data.slice(end_of_last_message);
+
+					console.log('[parser] Sliced data            : ', end_of_last_message, _self._buffer);
+				}
+			}
+
+			//console.log('[parser]', 'Buffered messages size : ', _self._buffer.length);
+		}
+	},
 
 	ye_olde_parser : function() {
 		console.log('[parser] Current chunk          : ', chunk);

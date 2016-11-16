c3bce3afcaaa26c4000dfef64e69d2923f4043d7
kdm <kennygprs@gmail.com>
2016-11-15 22:06:57 -0500

    Update

--- ibus/ibus-interface.js
+++ ibus/ibus-interface.js
@@ -21,7 +21,7 @@ var ibus_interface = function(omnibus) {
 		//autoOpen : false,
 		lock     : false,
 		parity   : 'even',
-		parser   : ibus_parser.parser, 
+		parser   : ibus_protocol.parser, 
 		rtscts   : true,
 	});
 

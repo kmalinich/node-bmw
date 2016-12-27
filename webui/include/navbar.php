84b8c524c644dc9424b902292efdb0cbd918f8ce
kdm <kmalinich@luxotticaretail.com>
2016-12-27 15:43:03 -0500

    Update

--- webui/include/navbar.php
+++ webui/include/navbar.php
@@ -13,11 +13,11 @@
 		</div>
 		<div class="collapse navbar-collapse" id="navbar">
 			<ul class="nav navbar-nav">
-				<li><a href="/gm">GM</a></li> 
-				<li><a href="/ike">IKE</a></li> 
-				<li><a href="/lcm">LCM</a></li> 
-				<li><a href="/status">Status</a></li> 
-				<li><a href="/ws-ibus">IBUS websocket</a></li> 
+				<li><a href="/gm">GM</a></li>
+				<li><a href="/ike">IKE</a></li>
+				<li><a href="/lcm">LCM</a></li>
+				<li><a href="/status">Status</a></li>
+				<li><a href="/ws-ibus">IBUS websocket</a></li>
 			</ul>
 		</div>
 	</div>

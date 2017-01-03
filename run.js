cb9cc6f239e5660844a2df047d9ffdaaa4a2a928
kdm <kennygprs@gmail.com>
2017-01-03 03:46:50 -0500

    Fix api startup/shutdown

--- run.js
+++ run.js
@@ -1,48 +1,48 @@
 #!/usr/bin/env node
 
 // npm libraries
-var http              = require('http');
-var http_dispatcher   = require('httpdispatcher');
-var query_string      = require('querystring');
-var url               = require('url');
-var wait              = require('wait.for');
-var lib_socket_server = require('./lib/socket-server.js')
+const http              = require('http');
+const http_dispatcher   = require('httpdispatcher');
+const query_string      = require('querystring');
+const url               = require('url');
+const lib_socket_server = require('./lib/socket-server.js')
+const wait              = require('wait.for');
 
 // IBUS libraries
-var data_handler   = require('./ibus/data-handler.js');
-var ibus_interface = require('./ibus/ibus-interface.js');
+const data_handler   = require('./ibus/data-handler.js');
+const ibus_interface = require('./ibus/ibus-interface.js');
 
 // Other custom libraries
-var kodi = require('./lib/kodi.js');
-var HDMI = require('./lib/HDMI.js');
+const kodi = require('./lib/kodi.js');
+const HDMI = require('./lib/HDMI.js');
 
 // Module libraries
-var ABG  = require('./modules/ABG.js');
-var ANZV = require('./modules/ANZV.js');
-var BMBT = require('./modules/BMBT.js');
-var CCM  = require('./modules/CCM.js');
-var CDC  = require('./modules/CDC.js');
-var DSP  = require('./modules/DSP.js');
-var DSPC = require('./modules/DSPC.js');
-var EWS  = require('./modules/EWS.js');
-var GM   = require('./modules/GM.js');
-var GT   = require('./modules/GT.js');
-var HAC  = require('./modules/HAC.js');
-var IHKA = require('./modules/IHKA.js');
-var IKE  = require('./modules/IKE.js');
-var LCM  = require('./modules/LCM.js');
-var MFL  = require('./modules/MFL.js');
-var MID  = require('./modules/MID.js');
-var PDC  = require('./modules/PDC.js');
-var RAD  = require('./modules/RAD.js');
-var RLS  = require('./modules/RLS.js');
-var SES  = require('./modules/SES.js');
-var SHD  = require('./modules/SHD.js');
-var TEL  = require('./modules/TEL.js');
-var VID  = require('./modules/VID.js');
+const ABG  = require('./modules/ABG.js');
+const ANZV = require('./modules/ANZV.js');
+const BMBT = require('./modules/BMBT.js');
+const CCM  = require('./modules/CCM.js');
+const CDC  = require('./modules/CDC.js');
+const DSP  = require('./modules/DSP.js');
+const DSPC = require('./modules/DSPC.js');
+const EWS  = require('./modules/EWS.js');
+const GM   = require('./modules/GM.js');
+const GT   = require('./modules/GT.js');
+const HAC  = require('./modules/HAC.js');
+const IHKA = require('./modules/IHKA.js');
+const IKE  = require('./modules/IKE.js');
+const LCM  = require('./modules/LCM.js');
+const MFL  = require('./modules/MFL.js');
+const MID  = require('./modules/MID.js');
+const PDC  = require('./modules/PDC.js');
+const RAD  = require('./modules/RAD.js');
+const RLS  = require('./modules/RLS.js');
+const SES  = require('./modules/SES.js');
+const SHD  = require('./modules/SHD.js');
+const TEL  = require('./modules/TEL.js');
+const VID  = require('./modules/VID.js');
 
 // Everything connection object
-var omnibus = {};
+const omnibus = {};
 
 omnibus.bus_modules     = require('./lib/bus-modules.js');
 omnibus.status          = require('./lib/status.js');  // Vehicle status object
@@ -79,9 +79,11 @@ omnibus.HDMI = new HDMI(omnibus);
 omnibus.api_server    = http.createServer(api_handler);
 omnibus.socket_server = new lib_socket_server(omnibus);
 
-// Server ports
-var api_port       = 3001;
-var websocket_port = 3002;
+// HTTP/WS config
+var api_port        = 3001;
+var api_socket_key_last = 0;
+var api_socket_map      = {};
+var websocket_port  = 3002;
 
 
 // Global startup
@@ -92,13 +94,13 @@ function startup() {
     console.log('[      API] Started up, port %s', api_port);
     // Start WebSocket server
     omnibus.socket_server.startup(() => {
-			console.log('[       WS] Started up');
+      console.log('[       WS] Started up');
       // Start HDMI CEC
       omnibus.HDMI.startup(() => {
-				console.log('[     HDMI] Started up');
+        console.log('[     HDMI] Started up');
         // Start IBUS connection
         omnibus.ibus_connection.startup(() => {
-					console.log('[     INTF] Started up');
+          console.log('[     INTF] Started up');
         });
       });
     });
@@ -110,12 +112,12 @@ function shutdown() {
   console.log('[ node-bmw] Shutting down');
   // Close serial port if open, and exit process
   omnibus.ibus_connection.shutdown(() => {
-		console.log('[     INTF] Shut down');
+    console.log('[     INTF] Shut down');
     omnibus.HDMI.shutdown(() => {
-			console.log('[     HDMI] Shut down');
-      // socket server?
+      console.log('[     HDMI] Shut down');
+      // socket server? .. nah, it's the api server. er..
       omnibus.api_server.close(() => {
-				console.log('[      API] Shut down');
+        console.log('[      API] Shut down');
         process.exit();
       });
     });
@@ -124,9 +126,48 @@ function shutdown() {
 
 
 // Port 3001 listener for POST requests to modules
-// This should be moved into it's own object
+// This REALLY REALLY REALLY REALLY should be moved into it's own object
 
 var dispatcher = new http_dispatcher();
+
+function startup_api_server(callback) {
+  // error handling breh
+  omnibus.api_server.listen(api_port, () => {
+    console.log('[      API] Started up, port %s', api_port);
+
+    omnibus.api_server.on('connection', (api_socket) => {
+      // Generate a new, unique api_socket-key
+      var api_socket_key = ++api_socket_key_last;
+
+      // Add api_socket when it is connected
+      api_socket_map[api_socket_key] = api_socket;
+
+      // Remove api_socket when it is closed
+      api_socket.on('close', () => {
+        delete api_socket_map[api_socket_key];
+      });
+    });
+  });
+}
+
+// Close API server and kill the sockets
+function shutdown_api_server(callback) {
+  console.log('[      API] Shutting down');
+
+  // Loop through all sockets and destroy them
+  Object.keys(api_socket_map).forEach((api_socket_key) => {
+    socket_map[api_socket_key].destroy();
+  });
+
+  // Tell server to close
+  omnibus.api_server.close();
+
+  // API server close event
+  omnibus.api_server.on('close', () => {
+    // Only call back if callback is a function
+    if (typeof callback === 'function') { callback(); }
+  });
+}
 
 // API handler function
 function api_handler(request, response) {

01d17d996d618f0514014b804752bc421c362b5e
kdm <kmalinich@luxotticaretail.com>
2017-01-05 21:33:44 -0500

    CSS/WebUI

--- webui/include/css.php
+++ webui/include/css.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="/css/bootstrap.css"/>
 <link rel="stylesheet" href="/css/font-awesome.css"/>
 <link rel="stylesheet" href="/css/bootstrap-slider.css"/>
+<link rel="stylesheet" href="/css/font-bmw.css"/>
 <link rel="stylesheet" href="/css/font-sfns.css"/>
 <link rel="stylesheet" href="/css/node-bmw.css"/>

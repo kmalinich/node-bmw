bbb00d3ca0efaa45173ce38220579f7404404732
kdm <kennygprs@gmail.com>
2017-03-31 19:31:13 -0400

    Fixed light timeout on ignition off

--- modules/LCM.js
+++ modules/LCM.js
@@ -482,13 +482,21 @@ function io_set(packet) {
 
   // Request the IO status after
   //omnibus.LCM.request('io-status');
-  console.log('[node::LCM] reset()');
 }
 
 // Make things.. how they should be?
 function reset() {
   var reset_dimmer_val; // Determine dimmer value from config, depending if lowbeams are on
   var reset_standing;   // Determine standing lights on/off from lowbeam (it's either one or the other)
+
+	if (status.vehicle.ignition !== 'run' || config.lights.auto !== true) {
+		// Not in run: turn off all lights
+		console.log('[node::LCM] reset(): Vehicle off; disabling');
+		io_encode({});
+		return;
+	}
+
+	console.log('[node::LCM] reset(): Vehicle running');
 
   switch (status.lights.auto.lowbeam) {
     case true:

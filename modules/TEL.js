4e4e96a34aa040685e6deb6ea5324d9fa3966ff4
kdm <kennygprs@gmail.com>
2017-04-22 00:52:33 -0400

    Added TEL docs and todo

--- modules/TEL.js
+++ modules/TEL.js
@@ -8,6 +8,39 @@ function parse_out(data) {
 			data.value   = 'indicator status';
 			break;
 
+		case 0x2C: // Broadcast: Telephone status
+			data.command = 'bro';
+			data.value   = 'telephone status TODO';
+
+			// Bit0
+			// 0 = Handsfree off
+			// 1 = Handsfree on
+
+			// Bit1
+			// 0 = Telephone menu on board monitor or MID
+			// 1 = active call
+
+			// Bit2
+			// 0 = Incoming call : false
+			// 1 = Incoming call : true
+
+			// Bit3
+			// 0 - Phone screen disabled : false
+			// 1 = Phone screen disabled : true
+
+			// Bit4
+			// 0 = Phone off
+			// 1 = Phone on
+
+			// Bit5
+			// 0 = Phone inactive
+			// 1 = Phone active
+
+			// Bit6
+			// 0 = Adapter not installed
+			// 1 = Adapter installed
+			break;
+
 		default:
 			data.command = 'unk';
 			data.value   = Buffer.from(data.msg);
